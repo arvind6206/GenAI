3232f522ceae4d5558c495853d2f2a355d1bbb3e
arvind6206 <vishwkarmaarvind1222@gmail.com>
2026-08-16 10:38:48 +0530

    llm

---
+++ Demo2/index.js
@@ -0,0 +1,17 @@
+import dotenv from 'dotenv'
+dotenv.config()
+import OpenAI from 'openai'
+
+console.log(process.env.GROQ_API_KEY)
+
+const client = new OpenAI({
+    apiKey: process.env.GROQ_API_KEY,
+    baseURL: "https://api.groq.com/openai/v1"
+})
+
+const response = await client.responses.create({
+    model: 'openai/gpt-oss-20b',
+    input: "Explain the async function in js"
+})
+
+console.log(response.output_text)
